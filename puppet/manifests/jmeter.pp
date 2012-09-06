f0a073d1139e0797bfcdc9908ca231a41ba5bb05
wulff <wulff@ratatosk.net>
2012-09-06 17:54:27 +0200

    updated jmeter submodule and cleaned up main manifest

--- puppet/manifests/jmeter.pp
+++ puppet/manifests/jmeter.pp
@@ -5,8 +5,7 @@ Exec { path => '/usr/sbin/:/sbin:/usr/bin:/bin' }
 File { owner => 'root', group => 'root' }
 
 # create a stage to make sure apt-get update is run before all other tasks
-stage { 'bootstrap': before => Stage['requirements'] }
-stage { 'requirements': before => Stage['main'] }
+stage { 'bootstrap': before => Stage['main'] }
 
 class jmeter::bootstrap {
   # we need an updated list of sources before we can apply the configuration
@@ -15,35 +14,17 @@ class jmeter::bootstrap {
 	}
 }
 
-class jmeter::requirements {
-  package { ['htop', 'unzip']:
+class jmeter::install {
+  package { 'htop':
     ensure => present,
   }
-}
 
-class jmeter::install {
   class { 'jmeter::server': }
-
-  exec { 'download-jmeter-plugins':
-    command => 'wget -P /root http://jmeter-plugins.googlecode.com/files/JMeterPlugins-0.5.4.zip',
-    creates => '/root/JMeterPlugins-0.5.4.zip'
-  }
-
-  exec { 'install-jmeter-plugins':
-    command => 'unzip -q -d JMeterPlugins JMeterPlugins-0.5.4.zip  && mv JMeterPlugins/JMeterPlugins.jar /usr/share/jmeter/lib/ext',
-    cwd     => '/root',
-    creates => '/usr/share/jmeter/lib/ext/JMeterPlugins.jar',
-    require => [Exec['install-jmeter'], Exec['download-jmeter-plugins']],
-    notify  => Service['jmeter'],
-  }
 }
 
 class jmeter::go {
   class { 'jmeter::bootstrap':
     stage => 'bootstrap',
-  }
-  class { 'jmeter::requirements':
-    stage => 'requirements',
   }
   class { 'jmeter::install': }
 }
